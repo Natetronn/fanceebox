80204d8a5a8623362e0a97d9fc0f846e29c327e8
Nathan Doyle <natetronn@gmail.com>
2011-08-07 19:31:13 -0700

    Initial commit

---
+++ system/expressionengine/third_party/fanceebox/acc.fanceebox.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Fanceebox Accessory
+ *
+ * @package			Fanceebox
+ * @version			1.0.4
+ * @author			Nathan Doyle <@natetronn>
+ * @copyright		Copyright (c) 2011 Cosmos Web Works, LLC
+ * @license			MIT  http://opensource.org/licenses/mit-license.php
+ * @link			https://github.com/Natetronn/fanceebox
+ */
+ 
+class fanceebox_acc 
+{
+	var $name			= 'Fanceebox';
+	var $id				= 'fanceebox';
+	var $version	 	= '1.0.4';
+	var $description	= 'Adds the Fancybox "lightbox" popup to the control panel for use with your custom fields\' labels';
+	var $sections	 	= array();
+	
+	/**
+	 * Constructor
+	 */
+	function __construct()
+	{
+		$this->EE =& get_instance();
+
+		$theme_folder_url = $this->EE->config->item('theme_folder_url');
+
+		if (substr($theme_folder_url, -1) != '/')
+			$theme_folder_url .= '/';
+
+		$theme_folder_url .= "third_party/fanceebox/";
+		
+		$this->EE->cp->load_package_js('jquery.easing-1.3.pack');
+		$this->EE->cp->load_package_js('jquery.mousewheel-3.0.4.pack');
+		$this->EE->cp->load_package_js('jquery.fancybox-1.3.4.pack');
+		$this->EE->cp->load_package_js('jquery.fanceebox');
+		$this->EE->cp->add_to_head('<link rel="stylesheet" type="text/css" href="'.$theme_folder_url.'fancybox/jquery.fancybox-1.3.4.css" />');
+	}
+
+	/**
+	 * Set Sections
+	 *
+	 * Set content for the accessory
+	 *
+	 * @access	public
+	 * @return	void
+	 */
+
+
+	function set_sections()
+	{
+		$this->sections[] ='<script type="text/javascript">$("#accessoryTabs .' . $this->id  . '").parent("li").hide();</script>';
+		
+	}
+	
+}
+// END CLASS
---
+++ system/expressionengine/third_party/fanceebox/javascript/jquery.fanceebox.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+	/* Single image with some animations and a few extras */
+	
+	$("a.fancy_image").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false,
+		'autoScale'		:	false,
+		'scrolling'		: 	'auto'
+	});
+	
+	/* Using custom settings */
+	
+	$("a.fancy_inline").fancybox({
+		'transitionIn'		:	'elastic',
+		'transitionOut'		:	'elastic',
+		'speedIn'			:	600, 
+		'speedOut'			:	200, 
+		'overlayShow'		:	false,
+		'autoScale'			:	false,
+		'hideOnContentClick': 	true,
+		'scrolling'			: 	'auto'
+	});
+
+	/* For a gallery of multiple items use the same rel tag on each image you want in the gallery - example rel="group1" */
+	
+	$("a.fancy_group").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false,
+		'autoScale'		:	false,
+		'scrolling'		: 	'auto'
+	});
+	
+	/* This is for the iframe */
+	
+	$("a.fancy_iframe").fancybox({
+		'transitionIn'	:	'elastic',
+		'transitionOut'	:	'elastic',
+		'speedIn'		:	600, 
+		'speedOut'		:	200, 
+		'overlayShow'	:	false,
+		'width' 		:	'75%',
+		'height' 		:	'75%',
+		'autoScale' 	:	false,
+		'type' 			:	'iframe'
+	});
+});
